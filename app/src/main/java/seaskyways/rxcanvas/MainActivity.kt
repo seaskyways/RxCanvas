a5a5364777e0f444b51bca3017f2bffd9952acd8
Ahmad Al-Sharif <ahmadalsharif.dev@gmail.com>
2017-01-24 09:46:12 +0200

    restart game ...

--- app/src/main/java/seaskyways/rxcanvas/MainActivity.kt
+++ app/src/main/java/seaskyways/rxcanvas/MainActivity.kt
@@ -3,12 +3,21 @@ package seaskyways.rxcanvas
 import android.os.Bundle
 import com.trello.rxlifecycle2.android.ActivityEvent
 import com.trello.rxlifecycle2.components.RxActivity
+import io.reactivex.BackpressureStrategy
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.subscribers.DisposableSubscriber
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.find
 
 class MainActivity : RxActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startLogic()
+        
+    }
+    
+    fun startLogic() {
         setContentView(R.layout.activity_main)
         
         val circleView = find<CircleView>(R.id.circle_view)
@@ -24,5 +33,35 @@ class MainActivity : RxActivity() {
                 .subscribe {
                     circleView.shouldContinue = false
                 }
+        
+        circleView.userBallOverlapSubject
+                .filter { it }
+                .toFlowable(BackpressureStrategy.DROP)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : DisposableSubscriber<Boolean>() {
+                    override fun onError(t: Throwable?) {
+                        
+                    }
+                    
+                    override fun onNext(t: Boolean?) {
+                        alert("Restart Game ?") {
+                            okButton {
+                                circleView.shouldContinue = true
+                                circleView.userBallOverlapSubject.onComplete()
+                                startLogic()
+                                request(1)
+                                
+                            }
+                            noButton()
+                            show()
+                        }
+                    }
+                    
+                    
+                    override fun onComplete() {
+                        
+                    }
+                    
+                })
     }
 }
---
+++ app/src/test/java/seaskyways/rxcanvas/RxTest.kt
@@ -0,0 +1,59 @@
+package seaskyways.rxcanvas
+
+import io.reactivex.Observable
+import io.reactivex.schedulers.Schedulers.*
+import org.junit.Test
+import java.lang.Thread.currentThread
+import java.lang.Thread.sleep
+
+/**
+ * Created by User1 on 23/01 - Jan/17.
+ */
+class RxTest {
+    
+    @Test
+    @Throws
+    fun groupByTest() {
+//
+//        Observable.create<Int> { emitter ->
+//            for (i in 0..100) {
+//                emitter.onNext((Math.random() * i * 100).toInt())
+////                Thread.sleep(50)
+//            }
+//            emitter.onComplete()
+//        }
+//                .subscribeOn(computation())
+//                .groupBy { it.compareTo(1000) }
+//                .flatMapSingle(GroupedObservable<Int, Int>::toList)
+//                .doOnNext { it.sort() }
+//                .observeOn(trampoline())
+//                .subscribe(::println)
+//
+//        sleep(1000)
+    }
+    /*
+    * [0, 20, 37, 46, 67, 108, 148, 152, 185, 203, 211, 257, 276, 308, 351, 380, 432, 439, 531, 550, 565, 576, 652, 680, 736, 767, 772, 802, 811, 865, 891, 897, 951, 961, 995]
+    * [1000]
+    * [1070, 1125, 1242, 1263, 1264, 1414, 1487, 1564, 1606, 1655, 1694, 1723, 1900, 1920, 1951, 1956, 1978, 2022, 2042, 2159, 2358, 2392, 2552, 2562, 2620, 2822, 2890, 2934, 2942, 3034, 3119, 3180, 3334, 3373, 3398, 3806, 3826, 3988, 4107, 4202, 4237, 4304, 4329, 4458, 4545, 4608, 4776, 4782, 4956, 4989, 5004, 5361, 5385, 5597, 5626, 6199, 6799, 6824, 6938, 7624, 8016, 8545, 8596, 8843, 9424]
+    * Process finished with exit code 0
+    * */
+    
+    @Test
+    @Throws
+    fun schedulersTest(){
+        fun printThread() = println("Thread name : ${currentThread().name}")
+        printThread()
+        Observable.just(1)
+                .doOnNext { printThread() }
+                .subscribeOn(newThread())
+                .doOnNext { printThread() }
+                .observeOn(computation())
+                .doOnNext { printThread() }
+                .observeOn(io())
+                .doOnNext { printThread() }
+                .subscribe()
+        
+        sleep(1000)
+    }
+    
+}
