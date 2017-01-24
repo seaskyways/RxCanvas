7aeb25bb141eec9f294108d356ee7d2495b196cb
seaskyways <seaskyways@gmail.com>
2017-01-24 23:23:26 +0200

    more more features

--- app/src/main/java/seaskyways/rxcanvas/CircleView.kt
+++ app/src/main/java/seaskyways/rxcanvas/CircleView.kt
@@ -12,6 +12,7 @@ import io.reactivex.schedulers.Schedulers.*
 import io.reactivex.subjects.*
 import io.reactivex.subscribers.DisposableSubscriber
 import org.jetbrains.anko.*
+import seaskyways.rxcanvas.Renderable.Companion.rederable
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.*
 
@@ -25,18 +26,50 @@ class CircleView : View, AnkoLogger {
     
     //    val refresherSubject = PublishSubject.create<Any>()!!
     
+    var userBallRadius = dip(30)
+    
     
     private val Subjects = object {
         val refresh = PublishSubject.create<Unit>()!!
     }
+    private val Paints = object {
+        val textPaint = Paint().apply {
+            color = Color.BLACK
+            textAlign = Paint.Align.CENTER
+            textSize = sp(20).toFloat()
+        }
+        
+        val bottomLeftText = Paint(textPaint).apply {
+            textAlign = Paint.Align.LEFT
+        }
+        
+        
+        val circlePaint = Paint().apply {
+            color = Color.BLACK
+            style = Paint.Style.STROKE
+            strokeWidth = stroke
+        }
+        
+        var userCirclePaint = Paint(circlePaint).apply {
+            color = Color.GREEN
+        }
+        
+        val overlapPaint = Paint(circlePaint).apply {
+            color = Color.RED
+        }
+    }
+    private val Observables = object {
+        val userPointSubject = BehaviorSubject.create<PointF>()
+    }
     
-//    val currPointSubject = BehaviorSubject.create<MotionEvent>()!!
+    
+    //    val currPointSubject = BehaviorSubject.create<MotionEvent>()!!
     val score = AtomicInteger(0)
     
     val ballsObservable = ReplaySubject.create<Ball>()!!
     val ballDisposalSubject: BehaviorSubject<Int> = BehaviorSubject.create<Int>()
     
-    val userBallOverlapSubject: BehaviorSubject<Boolean> = BehaviorSubject.createDefault(false)
+    val userBallOverlapSubject: BehaviorSubject<Boolean> = BehaviorSubject.create()
     
     var shouldContinue = true
     
@@ -48,10 +81,17 @@ class CircleView : View, AnkoLogger {
     
     var currLifecycle: ActivityEvent = ActivityEvent.PAUSE
     
-    var currX: Float = 0f
-    var currY: Float = 0f
+    //    var currX: Float = 0f
+//    var currY: Float = 0f
+    val userPoint = PointF(0f, 0f)
     
-    inner class Ball(val id: Int = 1, val y: Int, val radius: Long = 100L, x: Long? = null) : Renderable {
+    init {
+        Observables.userPointSubject
+                .sample(2, TimeUnit.MILLISECONDS)
+                .subscribe { userPoint.set(it.x, it.y) }
+    }
+    
+    inner class Ball(val id: Int = 1, val y: Int, val radius: Long = dip(25).toLong(), x: Long? = null) : Renderable {
         val minTime = (1000000000L * (Math.log10(id.toDouble()))).toLong()
         val maxTime = (3000000000L * (Math.log10(id.toDouble()))).toLong()
         
@@ -90,10 +130,10 @@ class CircleView : View, AnkoLogger {
                             ,
                             {
                                 //                                warn("CAN DISPOSE !")
-                                ballDisposalSubject.onNext(id)
                                 canDispose = true
                                 isAnimating = false
                                 score.incrementAndGet()
+                                ballDisposalSubject.onNext(id)
                             })
         }
         
@@ -107,41 +147,14 @@ class CircleView : View, AnkoLogger {
         fun isIntersecting(another: Ball): Boolean {
             val distanceXS = Math.pow((x.get() - another.x.get()).toDouble(), 2.0)
             val distanceYS = Math.pow((y - another.y).toDouble(), 2.0)
-            val radii = radius + another.radius
-            return distanceXS + distanceYS <= (radii * radii)
+            val radiiS = Math.pow((radius + another.radius).toDouble(), 2.0)
+            return distanceXS + distanceYS <= (radiiS)
         }
     }
     
     val stroke = 30f
     val randY: Int get() = (measuredHeight * Math.random()).toInt()
     
-    
-    private val Paints = object {
-        val textPaint = Paint().apply {
-            color = Color.BLACK
-            textAlign = Paint.Align.CENTER
-            textSize = sp(20).toFloat()
-        }
-        
-        val bottomLeftText = Paint(textPaint).apply {
-            textAlign = Paint.Align.LEFT
-        }
-        
-        
-        val circlePaint = Paint().apply {
-            color = Color.BLACK
-            style = Paint.Style.STROKE
-            strokeWidth = stroke
-        }
-        
-        val userCirclePaint = Paint(circlePaint).apply {
-            color = Color.GREEN
-        }
-        
-        val overlapPaint = Paint(circlePaint).apply {
-            color = Color.RED
-        }
-    }
     
     val refresher: Flowable<Unit>
     
@@ -167,23 +180,8 @@ class CircleView : View, AnkoLogger {
     }
     
     init {
-//        refresher = Observable.interval(15, TimeUnit.MILLISECONDS)
-//                .filter { shouldContinue }
-//                .observeOn(mainThread())
-//
         ballsObservable
                 .subscribe { Subjects.refresh.onNext(Unit) }
-
-//        Flowable.create<Unit>({ e ->
-//            Subjects.refresh
-//                    .subscribeOn(newThread())
-//                    .subscribe {
-//                        if (e.requested() > 0)
-//                            e.onNext(Unit)
-//                    }
-//        }, BackpressureStrategy.MISSING)
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(refreshFlowableObserver)
         
         refresher = Subjects.refresh
                 .subscribeOn(newThread())
@@ -203,10 +201,10 @@ class CircleView : View, AnkoLogger {
                 .subscribe({ ballsObservable.onNext(Ball(it, randY)) }, Throwable::printStackTrace)
         
         ballDisposalSubject
-                .subscribeOn(newThread())
-                .flatMap {
+                .observeOn(newThread())
+                .flatMap { ballId ->
                     (0 until currentBalls.length())
-                            .filter { i -> currentBalls[i]?.id ?: 0 == it }
+                            .filter { i -> currentBalls[i]?.id ?: 0 == ballId }
                             .forEach { return@flatMap Observable.just(it) }
                     return@flatMap Observable.empty<Int>()
                 }
@@ -229,31 +227,19 @@ class CircleView : View, AnkoLogger {
                 }
         
         refresher
-                .delay(1, TimeUnit.MILLISECONDS)
+//                .delay(1, TimeUnit.MILLISECONDS)
                 .observeOn(Schedulers.single())
                 .subscribe {
                     val overlap = currentBalls
-                            .indexOfFirst { it?.isIntersecting(Ball(y = currY.toInt(), x = currX.toLong())) ?: false } != -1
+                            .indexOfFirst { it?.isIntersecting(Ball(y = userPoint.y.toInt(), x = userPoint.x.toLong(), radius = userBallRadius.toLong())) ?: false } != -1
                     userBallOverlapSubject.onNext(overlap)
                 }
         
-//        currPointSubject
-//                .subscribeOn(single())
-//                .map {
-//                    object {
-//                        val x = it.rawX
-//                        val y = it.rawY
-//                    }
-//                }
-//                .doOnNext { isDispatching = true }
-//                .sample(100, TimeUnit.MICROSECONDS)
-//                .observeOn(Schedulers.single())
-//                .subscribe {
-//                    currX = it.x
-//                    currY = it.y
-//                    Subjects.refresh.onNext(Unit)
-//                    isDispatching = false
-//                }
+        userBallOverlapSubject
+                .observeOn(single())
+                .filter { it }
+                .subscribe { Paints.userCirclePaint = Paints.overlapPaint }
+        
     }
     
     fun nearestNullIndex(): Int {
@@ -266,10 +252,8 @@ class CircleView : View, AnkoLogger {
         return index
     }
     
-    val bottomLeftText: Renderable = object : Renderable {
-        override fun render(canvas: Canvas) {
-            canvas.drawText("Score : ${score.get()}", 50f, measuredHeight.toFloat() - 50, Paints.bottomLeftText)
-        }
+    val bottomLeftText: Renderable = rederable { canvas ->
+        canvas.drawText("Score : ${score.get()}", 50f, measuredHeight.toFloat() - 50, Paints.bottomLeftText)
     }
     
     fun clearDisposableBalls() {
@@ -286,22 +270,8 @@ class CircleView : View, AnkoLogger {
 //                }
     }
     
-    
-//    var isDispatching = false
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-//        if (isDispatching) return false
-        
-//        currPointSubject.onNext(event)
-
-//        currPointSubject.onNext(PointF(event.x, event.y))
-//        if (isDispatchingTouchEvent) {
-//            warn("IsDispatching is true")
-//            return false
-//        }
-//        isDispatchingTouchEvent = true
-        currX = event.x
-        currY = event.y
-//        isDispatchingTouchEvent = false
+        Observables.userPointSubject.onNext(PointF(event.x, event.y))
         return true
     }
     
@@ -315,8 +285,7 @@ class CircleView : View, AnkoLogger {
                     .forEach {
                         it?.render(canvas)
                     }
-            val p = if (userBallOverlapSubject.values.contains(true)) Paints.overlapPaint else Paints.userCirclePaint
-            drawCircle(currX, currY, 100f, p)
+            drawCircle(userPoint.x, userPoint.y, userBallRadius.toFloat(), Paints.userCirclePaint)
         }
         refreshFlowableObserver.requestMore(1)
     }
--- app/src/main/java/seaskyways/rxcanvas/Renderable.kt
+++ app/src/main/java/seaskyways/rxcanvas/Renderable.kt
@@ -6,5 +6,13 @@ import android.graphics.Canvas
  * Created by Ahmad on 21/01 Jan/2017.
  */
 interface Renderable {
-    fun render(canvas : Canvas)
+    fun render(canvas: Canvas)
+    
+    companion object {
+        fun rederable(r: (Canvas) -> Unit): Renderable = object : Renderable {
+            override fun render(canvas: Canvas) {
+                r(canvas)
+            }
+        }
+    }
 }
